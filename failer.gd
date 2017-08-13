a6cb04370e95e90bf39c770f945b93dde7f26c5b
Sebastian Ebert <kriet108@gmail.com>
2017-08-13 15:34:11 -0400

    Overhaul succeeder in line with previous commit and fix return value for failer.

--- failer.gd
+++ failer.gd
@@ -2,7 +2,7 @@ extends "res://addons/godot-behavior-tree-plugin/bt_base.gd"
 
 const BehvError = preload("res://addons/godot-behavior-tree-plugin/error.gd")
 
-#Decorator node - Always returns FAILED
+#Decorator node - Always returns FAILED if not running or errored
 func tick(tick):
 	if get_child_count() > 1:
 		return BehvError.new(self, "ERROR BehaviorFailer has more than one child")
@@ -10,4 +10,8 @@ func tick(tick):
 	# 0..1 children
 	for c in get_children():
 		var result = c._execute(tick)
+
+		if (typeof(result) == TYPE_OBJECT and result extends BehvError) or result == ERR_BUSY:
+			return result
+
 		return FAILED
