51bc1e148d3d6ac6660ac86527fd10b9e50e2d95
EvilKarut <EvilKarut@protonmail.com>
2019-07-15 17:50:49 +0200

    Deleted debug messages node in/out

--- addons/godot-behavior-tree-plugin/bt_base.gd
+++ addons/godot-behavior-tree-plugin/bt_base.gd
@@ -20,7 +20,6 @@ func _enter(tick):
 	enter(tick)
 
 func _open(tick):
-	print("opening node")
 	tick.openNode(self)
 	tick.blackboard.set('isOpen', true, tick.tree, self)
 	open(tick)
@@ -30,7 +29,6 @@ func _tick(tick):
 	return tick(tick)
 
 func _close(tick):
-	print("closing node")
 	tick.closeNode(self)
 	tick.blackboard.set('isOpen', false, tick.tree, self)
 	close(tick)
